2cb4be1bc5392ca2cade22ffa416a57bf9bd4b57
Daniel Rego Calvo <dregocal@emeal.nttdata.com>
2025-09-18 16:16:20 +0200

    Added use of credentials in the code

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,17 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        if (args.length > 0) {
+            System.out.println("Arguments passed:");
+            for (String arg : args) {
+                System.out.println(arg.length());
+                System.out.println(arg);
+            }
+        } else {
+            System.out.println("No arguments provided.");
+        }
+
     }
 
     public static int sumarNumeros(int a, int b){
